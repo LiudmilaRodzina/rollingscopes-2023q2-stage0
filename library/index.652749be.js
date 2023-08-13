558559260745b12a5f825bbb4687dc8c24190b27
Liudmila Rodzina <liudmilarodzina@gmail.com>
2023-08-13 23:23:29 +0300

    Updates

---
+++ library/index.652749be.js
@@ -0,0 +1,2 @@
+console.log("Add responsive 768px\nAdd adaptive 1440-640px");const e=document.querySelector(".navigation"),t=document.querySelector(".header__burger"),o=document.body;e&&t&&t.addEventListener("click",()=>{e.classList.toggle("active"),t.classList.toggle("active"),o.classList.toggle("lock")});
+//# sourceMappingURL=index.652749be.js.map
