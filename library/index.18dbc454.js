6cb4560cee9a714a605d344e63e192506aec226c
Liudmila Rodzina <liudmilarodzina@gmail.com>
2023-08-14 15:29:24 +0300

    Updates

--- library/index.18dbc454.js
+++ library/index.18dbc454.js
@@ -574,15 +574,28 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
-console.log("Привет!\n1. Вёрстка соответствует макету. Ширина экрана 768px +26 \n2. Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12 \n3. На ширине экрана 768рх реализовано адаптивное меню (с недочётами) +10\nИтого: 48\nЕсли есть возможность, пожалуйста, вернись к проверке позже, попробую доработать адаптивное меню. \nМой дискорд: Liudmila#7427.\nСпасибо! :)");
-// Burger-menu
-const navigation = document.querySelector(".navigation");
-const burgerBtn = document.querySelector(".header__burger");
-const body = document.body;
-if (navigation && burgerBtn) burgerBtn.addEventListener("click", ()=>{
-    navigation.classList.toggle("active");
-    burgerBtn.classList.toggle("active");
-// body.classList.toggle("lock");
+console.log("Привет!\n1. Вёрстка соответствует макету. Ширина экрана 768px +26 \n2. Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12 \n3. На ширине экрана 768рх реализовано адаптивное меню +12\nИтого: 50\nСпасибо! :)");
+// BURGER_MENU
+document.addEventListener("DOMContentLoaded", ()=>{
+    const navigation = document.querySelector(".navigation");
+    const burgerBtn = document.querySelector(".header__burger");
+    const navLinks = document.querySelectorAll(".navigation__link");
+    burgerBtn.addEventListener("click", ()=>{
+        navigation.classList.toggle("active");
+        burgerBtn.classList.toggle("active");
+    });
+    navLinks.forEach((link)=>{
+        link.addEventListener("click", ()=>{
+            navigation.classList.remove("active");
+            burgerBtn.classList.remove("active");
+        });
+    });
+    document.addEventListener("click", (e)=>{
+        if (!e.target.closest(".header__burger") && !e.target.closest(".navigation")) {
+            navigation.classList.remove("active");
+            burgerBtn.classList.remove("active");
+        }
+    });
 });
 
 },{}]},["gEwwu","1SICI"], "1SICI", "parcelRequire04be")
