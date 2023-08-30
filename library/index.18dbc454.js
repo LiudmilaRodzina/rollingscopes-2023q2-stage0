5133939454eefee32801023d327279abdb9c683c
Liudmila Rodzina <liudmilarodzina@gmail.com>
2023-08-30 22:06:13 +0300

    Updates

--- library/index.18dbc454.js
+++ library/index.18dbc454.js
@@ -574,9 +574,10 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
-console.log("Привет!\n1. Вёрстка соответствует макету. Ширина экрана 768px +26 \n2. Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12 \n3. На ширине экрана 768рх реализовано адаптивное меню +12\nИтого: 50\nСпасибо! :)");
-// BURGER_MENU
+"use strict";
+console.log("Выполнено: Ограниченная карусель в блоке About + 25 баллов");
 document.addEventListener("DOMContentLoaded", ()=>{
+    // BURGER_MENU
     const navigation = document.querySelector(".navigation");
     const burgerBtn = document.querySelector(".header__burger");
     const navLinks = document.querySelectorAll(".navigation__link");
@@ -595,6 +596,80 @@ document.addEventListener("DOMContentLoaded", ()=>{
             navigation.classList.remove("active");
             burgerBtn.classList.remove("active");
         }
+    });
+    ////////////////////////////////////////////////////////////////////////////
+    // SLIDER ABOUT
+    const sliderLine = document.querySelector(".slider__items");
+    const arrowLeft = document.querySelector(".arrow-left");
+    const arrowRight = document.querySelector(".arrow-right");
+    const dots = document.querySelectorAll(".pagination-dot");
+    let position = 0;
+    let dotIndex = 0;
+    const disableArrowRight = ()=>{
+        if (dotIndex === dots.length - 1) {
+            arrowRight.disabled = true;
+            arrowRight.classList.add("disabled");
+        } else {
+            arrowRight.disabled = false;
+            arrowRight.classList.remove("disabled");
+        }
+    };
+    const disableArrowLeft = ()=>{
+        if (dotIndex === 0) {
+            arrowLeft.disabled = true;
+            arrowLeft.classList.add("disabled");
+        } else {
+            arrowLeft.disabled = false;
+            arrowLeft.classList.remove("disabled");
+        }
+    };
+    const nextSlide = ()=>{
+        if (position < (dots.length - 1) * 47.5) {
+            position += 47.5;
+            dotIndex++;
+            disableArrowRight();
+            disableArrowLeft();
+        } else {
+            position = 0;
+            dotIndex = 0;
+        }
+        sliderLine.style.left = -position + "rem";
+        thisSlide(dotIndex);
+    };
+    const prevSlide = ()=>{
+        if (position > 0) {
+            position -= 47.5;
+            dotIndex--;
+            disableArrowRight();
+            disableArrowLeft();
+        } else {
+            position = (dots.length - 1) * 47.5;
+            dotIndex = dots.length - 1;
+        }
+        sliderLine.style.left = -position + "rem";
+        thisSlide(dotIndex);
+    };
+    const thisSlide = (index)=>{
+        for (let dot of dots)dot.classList.remove("dot-active");
+        dots[index].classList.add("dot-active");
+        disableArrowRight();
+        disableArrowLeft();
+    };
+    arrowRight.addEventListener("click", nextSlide);
+    arrowLeft.addEventListener("click", prevSlide);
+    arrowLeft.addEventListener("mouseover", ()=>{
+        if (dotIndex === 0) {
+            arrowLeft.disabled = true;
+            arrowLeft.classList.add("disabled");
+        }
+    });
+    dots.forEach((dot, index)=>{
+        dot.addEventListener("click", ()=>{
+            position = 47.5 * index;
+            sliderLine.style.left = -position + "rem";
+            dotIndex = index;
+            thisSlide(dotIndex);
+        });
     });
 });
 
