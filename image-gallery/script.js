71e8fb64a50570f75853dc9c6210e9b00951e710
Liudmila Rodzina <liudmilarodzina@gmail.com>
2023-10-02 19:20:05 +0300

    feat: add js functionality

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,28 +1,58 @@
-("use strict");
+"use strict";
 
-console.log(
-  `Привет! Прости, не успела выполнить задание до дедлайна. Если есть возможность, вернись, пожалуйста, к проверке через пару дней. Думаю, до среды должна справиться. Спасибо!`
-);
+console.log(`Привет!`);
 
 const accessKey = "l7EGpJ5GBstyjt1LhL6y90fLl37vTBUm8-itDtjrVe4";
-const galleryEl = document.getElementById("gallery");
 
-const loadImages = async (e) => {
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const gallery = document.querySelector(".gallery");
+const clearBtn = document.querySelector(".btn_clear");
+
+let keyword = "";
+let page = 1;
+
+document.addEventListener("load", loadRandom());
+
+async function loadRandom(
+  url = `https://api.unsplash.com/search/photos?per_page=18&page=${Math.round(
+    Math.random() * 100
+  )}&query=random&client_id=${accessKey}`
+) {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
   let images = "";
-  await fetch(
-    `https://api.unsplash.com/photos?per_page=12&page=${Math.round(
-      Math.random() * 1000
-    )}&client_id=${accessKey}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      if (data) {
-        data.forEach((img) => {
-          images += `<img src=${img.urls.regular} class="image">`;
-          galleryEl.style.display = "block";
-          galleryEl.innerHTML = images;
-        });
-      }
-    });
-};
-loadImages();
+  results.map((img) => {
+    images += `<img src=${img.urls.regular} class="image" alt="${img.alt_description}" draggable="false">`;
+    gallery.style.display = "flex";
+    gallery.innerHTML = images;
+  });
+}
+
+async function searchImages() {
+  let images = "";
+  keyword = searchInput.value;
+  let url = `https://api.unsplash.com/search/photos?per_page=18&page=${Math.round(
+    Math.random() * 100
+  )}&query=${keyword}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+  results.map((img) => {
+    images += `<img src=${img.urls.regular} class="image" alt="${img.alt_description}" draggable="false">`;
+    gallery.style.display = "flex";
+    gallery.innerHTML = images;
+  });
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+clearBtn.addEventListener("click", () => (searchInput.value = ""));
